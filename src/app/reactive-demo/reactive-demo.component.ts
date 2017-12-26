debe9303cbfff42a119ce4e18581d120eef9c7a3
MangeshG <Mangesh.g@intelgain.com>
2017-12-26 15:12:59 +0530

    Added New component

---
+++ src/app/reactive-demo/reactive-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reactive-demo',
+  templateUrl: './reactive-demo.component.html',
+  styleUrls: ['./reactive-demo.component.css']
+})
+export class ReactiveDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
